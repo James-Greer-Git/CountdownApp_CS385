148db9bc03e43892031357ecdf52cce355040c4d
James Greer <88616420+James-Greer-Git@users.noreply.github.com>
2021-11-25 20:28:55 +0000

    Update wordCheck.js

--- API?/wordCheck.js
+++ API?/wordCheck.js
@@ -1,23 +1,27 @@
 import React from "react";
+import {word} from "./word"
+//console.log(word);
 
 export default class FetchWord extends React.Component {
   state = {
-    loading : true
+    loading : true,
+    word : word
   };
 
 
   async componentDidMount () {
-    const URL = "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
+    const URL = "https://api.dictionaryapi.dev/api/v2/entries/en/" + this.state.word;
     const response = await fetch(URL);
     const data = await response.json();
-    console.log(data);
-    console.log(data[0].meanings)
+    //console.log(data);
+    //console.log(word);
+    console.log(data[0].meanings[0].definitions[0].definition)
   }
 
   render() {
     return (
       <div>
-        {this.state.loading ? <div>loading...</div> : <div>person</div>}
+        {this.state.loading ? <div>loading...</div> : <div>word</div>}
       </div>
     );
   }
