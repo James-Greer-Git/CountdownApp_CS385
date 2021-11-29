6f186418dba73f4aa9c01255af27a698867f4313
James Greer <88616420+James-Greer-Git@users.noreply.github.com>
2021-11-29 17:05:04 +0000

    Update wordCheck.js

--- API?/wordCheck.js
+++ API?/wordCheck.js
@@ -1,38 +1,41 @@
 import React, { Component } from "react";
 import {word} from "./word"
 //console.log(word);
-class App extends Component {
+class Definition extends Component {
   constructor(props) {
     super(props);
     console.log("in constructor");
 
     this.state = {
       apiData: [],
-      loading : true,
+      loading: true,
       definition: [],
-      word : word
+      word: ""
     };
   }
 
-  async componentDidMount () {
-    const URL = "https://api.dictionaryapi.dev/api/v2/entries/en/" + this.state.word;
+  async componentDidMount() {
+    const URL =
+      "https://api.dictionaryapi.dev/api/v2/entries/en/" + this.state.word;
     const response = await fetch(URL);
     const data = await response.json();
-    this.setState({apiData : data});
+    this.setState({ apiData: data });
     //console.log(data);
     //console.log(word);
-    this.setState({definition : data[0].meanings[0].definitions[0].definition});
+    this.setState({
+      definition: data[0].meanings[0].definitions[0].definition
+    });
     //console.log(data[0].meanings[0].definitions[0].definition)
-    this.setState({loading : false})
+    this.setState({ loading: false });
     //console.log(this.state.definition);
   }
 
-  render() {
+  /*render() {
     return (
       <div>
         {this.state.loading ? <div>loading...</div> : <div>Definition: {this.state.definition}</div>}
       </div>
     );
-  }
+  }*/
 }
-export default App;
+export default Definition;
