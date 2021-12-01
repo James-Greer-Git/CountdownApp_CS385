f109ae94a7a9707e386644fd6ee389cc9bac5b87
James Greer <88616420+James-Greer-Git@users.noreply.github.com>
2021-12-01 22:43:29 +0000

    Create mainWithWorkingTimer.js

---
+++ mainWithWorkingTimer.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import Definition from "./Definition"
+import SearchForm from "./SearchForm";
+//import Timer from "./Timer";
+import Timer2 from "./Timer2";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      len: 1,
+      SubmitPressed: false,
+      choice: "",
+      seconds: 60
+    };
+    //function start()=this.props.func(startTimer());
+    this.onSearchFormChange = this.onSearchFormChange.bind(this);
+    this.EasyButtonPress = this.EasyButtonPress.bind(this);
+    this.MediumButtonPress = this.MediumButtonPress.bind(this);
+    this.HardButtonPress = this.HardButtonPress.bind(this);
+  }
+  //localStorage localtimer=Timer;
+
+  EasyButtonPress() {
+    this.setState({ choice: "Easy", seconds: 60 });
+    //Timer2();
+    //console.log(Timer2);
+    //{this.props.startTimer};
+    // THIS DOESNT GIVE ERROR BUT DOESNT WORK new Timer();
+    //Timer.startTimer();
+    //var localtimer=Timer;
+    //localtimer.startTimer();
+    //trying to call start timer method
+    //var startTimer()=startTimer() from "./Timer";
+  }
+  MediumButtonPress() {
+    this.setState({ choice: "Medium", seconds: 45 });
+  }
+  HardButtonPress() {
+    this.setState({ choice: "Hard", seconds: 30 });
+  }
+  onSearchFormChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Countdown Game</h2>
+        <hr></hr>
+        {this.state.choice === "" ? (
+          <div>
+            <h3>Choose a Difficulty</h3>
+            <button onClick={this.EasyButtonPress}>Easy</button> 60 Second
+            Rounds
+            <hr></hr>
+            <button onClick={this.MediumButtonPress}>Medium</button> 45 Second
+            Rounds
+            <hr></hr>
+            <button onClick={this.HardButtonPress}>Hard</button> 30 Second
+            Rounds
+          </div>
+        ) : (
+          <div>
+            {this.state.SubmitPressed === true ? (
+              <div>True</div>
+            ) : (
+              <div>
+                You selected: {this.state.choice}
+                <SearchForm
+                  searchTerm={this.state.searchTerm}
+                  onChange={this.onSearchFormChange}
+                />
+                <Timer2
+                initialSeconds = {this.state.seconds}
+                />
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+export default App;
