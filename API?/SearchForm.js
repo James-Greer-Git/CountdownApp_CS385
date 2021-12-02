c116823ca417e6ef6b2f092f152f2147cdd79656
James Greer <88616420+James-Greer-Git@users.noreply.github.com>
2021-12-02 15:57:55 +0000

    Update SearchForm.js

--- API?/SearchForm.js
+++ API?/SearchForm.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Definition from "./Definition";
 class SearchForm extends Component {
   constructor(props) {
     super(props);
@@ -7,38 +6,34 @@ class SearchForm extends Component {
       searchTerm: "",
     };
     this.onSearchFormChange = this.onSearchFormChange.bind(this);
+    this.onSubmitButtonPress = this.onSubmitButtonPress.bind(this);
   }
-  onSearchFormChange(event){
-    this.setState({searchTerm: event.target.value});
-    return(
-      <Definition
-          word = {this.state.searchTerm}
-          />
-    )
+  onSearchFormChange(event) {
+    this.setState({ searchTerm: event.target.value });
+    this.setState({submitPress : false});
+    let sTerm = event.target.value;
+    let numChars = sTerm.length;
+    this.setState({ word : this.state.searchTerm })
+    this.setState({ len: numChars });
   }
-  SubmitPressed() {
-    console.log("Submit Button Pressed")
-    this.setState({SubmitPressed : true})
+  onSubmitButtonPress(){
+    this.setState({submitPress : true, word : this.state.searchTerm});
   }
   render() {
     const searchTermFromProps = this.props.searchTerm;
     const onChangeFromProps = this.props.onChange;
 
     return (
-      <div className="SearchFormForm">
+      <div className="AnswerForm">
         <hr />
-        Search Component:
         <form>
-          <b>Type your search here: </b>
+          <b>Type your answer here: </b>
           <input
             type="text"
             value={searchTermFromProps}
             onChange={onChangeFromProps}
           />
-          <div>
-            <button onClick = {this.SubmitPressed}>Submit</button>
-          </div>
-          {this.state.SubmitPressed ? <div>{this.state.searchTerm}</div> : <div></div>}
+          
         </form>
         <hr />
       </div>
