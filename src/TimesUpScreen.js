a8c32fa6fec3b4e166743e4317b43a848fa13eff
James Greer <88616420+James-Greer-Git@users.noreply.github.com>
2021-12-10 15:40:38 +0000

    Create TimesUpScreen.js

---
+++ src/TimesUpScreen.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+class TimesUpScreen extends Component{
+  render(){
+    return(
+      <div>Time's Up</div>
+    )
+  }
+}
+export default TimesUpScreen;
