ae6b6616e55359a2b156060330bd443be46882a4
James Greer <88616420+James-Greer-Git@users.noreply.github.com>
2021-12-10 14:32:39 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,64 +1,80 @@
-import "./App.css";
+//import "./App.css";
 import React, { Component } from "react";
-import Game from "./Game";
+import Timer2 from "./Timer2";
+import SearchForm from "./SearchForm";
+import RandomLetters from "./RandomLetters";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: "",
-      len: 1,
-      SubmitPressed: false,
       choice: "",
-      seconds: 60,
-      PlayPressed: false
+      seconds: 0,
+      playPressed: false,
+      submitPressed: false
     };
-    this.EasyButtonPress = this.EasyButtonPress.bind(this);
-    this.MediumButtonPress = this.MediumButtonPress.bind(this);
-    this.HardButtonPress = this.HardButtonPress.bind(this);
-    this.onHomePagePress = this.onHomePagePress.bind(this);
+    this.onBackToHomePagePress = this.onBackToHomePagePress.bind(this);
+    this.onPlayButtonPress = this.onPlayButtonPress.bind(this);
+    this.onSubmitButtonPress = this.onSubmitButtonPress.bind(this);
+    this.onEasyButtonPress = this.onEasyButtonPress.bind(this);
+    this.onMediumButtonPress = this.onMediumButtonPress.bind(this);
+    this.onHardButtonPress = this.onHardButtonPress.bind(this);
   }
-
-  EasyButtonPress() {
+  onEasyButtonPress() {
     this.setState({ choice: "Easy", seconds: 60 });
     console.log("Easy Button Pressed");
   }
-  MediumButtonPress() {
+  onMediumButtonPress() {
     this.setState({ choice: "Medium", seconds: 45 });
     console.log("Medium Button Pressed");
   }
-  HardButtonPress() {
+  onHardButtonPress() {
     this.setState({ choice: "Hard", seconds: 30 });
     console.log("Hard Button Pressed");
   }
-  onHomePagePress() {
-    this.setState({ choice: "", seconds: 60, PlayPressed: false });
+  onBackToHomePagePress() {
+    this.setState({ choice: "", playPressed: false, submitPressed: false });
+    console.log("Homepage Button Pressed");
+  }
+  onPlayButtonPress() {
+    this.setState({ playPressed: true });
+    console.log("Play Button Pressed");
+  }
+  onSubmitButtonPress() {
+    this.setState({ submitPressed: true });
   }
 
   render() {
+    var condition1 = this.state.choice === "";
+    var condition2 = !condition1 && !this.state.playPressed;
+    var condition3 = this.state.playPressed && !this.state.submitPressed;
+
     return (
-      <div className="App">
+      <div>
         <h2>Countdown Game</h2>
-        {this.state.choice === "" ? (
+        <button onClick={this.onBackToHomePagePress}>Back to homepage</button>
+        {condition1 && (
           <div>
             <h3>Choose a Difficulty</h3>
-            <button onClick={this.EasyButtonPress}>Easy</button> 60 Second
-            Rounds
-            <hr></hr>
-            <button onClick={this.MediumButtonPress}>Medium</button> 45 Second
-            Rounds
-            <hr></hr>
-            <button onClick={this.HardButtonPress}>Hard</button> 30 Second
-            Rounds
+            <button onClick={this.onEasyButtonPress}>Easy</button> 60 Second
+            Round<hr></hr>
+            <button onClick={this.onMediumButtonPress}>Medium</button> 45 Second
+            Round<hr></hr>
+            <button onClick={this.onHardButtonPress}>Hard</button> 30 Second
+            Round
           </div>
-        ) : (
+        )}
+        {condition2 && (
           <div>
-            <button onClick={this.onHomePagePress}>Back To Homepage</button>
-            <div>
-              <hr></hr>
-              <Game seconds={this.state.seconds} choice={this.state.choice} />
-              <hr></hr>
-            </div>
+            <button onClick={this.onPlayButtonPress}>Play</button>
+          </div>
+        )}
+        {this.state.playPressed && (
+          <div>
+            {condition3 && <Timer2 initialSeconds={this.state.seconds} />}
+            <b><RandomLetters/></b>
+            <p></p>
+            <SearchForm submitPressed={this.onSubmitButtonPress} />
           </div>
         )}
       </div>
