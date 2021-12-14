3b120b66d5f0a5b7f51ee6a6c482dd9231998a12
Emma-martin1 <91692660+Emma-martin1@users.noreply.github.com>
2021-12-14 18:47:28 +0100

    Create Timer2Updated.js

---
+++ Timer2Updated.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Timer2 = (props) => {
+  const { initialMinute = 0, initialSeconds = 0, setIsFinished } = props;
+  const [minutes, setMinutes] = useState(initialMinute);
+  const [seconds, setSeconds] = useState(initialSeconds);
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        setIsFinished();
+        clearInterval(myInterval);
+        if (minutes === 0) {
+          clearInterval(myInterval);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  }, [seconds, minutes]);
+
+  return (
+    <div>
+      {minutes === 0 && seconds === 0 ? null : (
+        <h1>
+          {" "}
+          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </h1>
+      )}
+    </div>
+  );
+};
+
+export default Timer2;
