c56b9de67424d1c1265c3d4349278e0bfb991578
James Greer <88616420+James-Greer-Git@users.noreply.github.com>
2022-01-20 12:09:26 +0000

    Update SearchForm.js

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,110 +1,147 @@
 import "./App.css";
-import Timer2 from "./Timer2";
 import React, { Component } from "react";
-import Definition from "./Definition";
-import SearchForm from "./SearchForm";
-//import Difficulty from "./Difficulty";
-
-class App extends Component {
+import Congratulations from "./Congratulations";
+import "bootstrap/dist/css/bootstrap.min.css";
+class SearchForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      //user input characters
       searchTerm: "",
-      len: 1,
+      //the amount of points the user has gotten from the characters they enetered
+      points: "",
+      //check if the user used the characters given
+      wrongInput: false,
+      wrongcharacter: true,
       SubmitPressed: false,
-      choice: "",
-      seconds: 60,
-      PlayPressed: false
+      //length of the word the user enetered
+      len: 0,
+      //the string the user submitted
+      word: ""
     };
+
     this.onSearchFormChange = this.onSearchFormChange.bind(this);
     this.onSubmitButtonPress = this.onSubmitButtonPress.bind(this);
-    this.EasyButtonPress = this.EasyButtonPress.bind(this);
-    this.MediumButtonPress = this.MediumButtonPress.bind(this);
-    this.HardButtonPress = this.HardButtonPress.bind(this);
-    this.onPlayButtonPress = this.onPlayButtonPress.bind(this);
-    this.onPlayAgainPress = this.onPlayAgainPress.bind(this);
-  }
-
-  EasyButtonPress() {
-    this.setState({ choice: "Easy", seconds: 60 });
-    console.log("Easy Button Pressed");
-  }
-  MediumButtonPress() {
-    this.setState({ choice: "Medium", seconds: 45 });
-    console.log("Medium Button Pressed");
-  }
-  HardButtonPress() {
-    this.setState({ choice: "Hard", seconds: 30 });
-    console.log("Hard Button Pressed");
-  }
-  onPlayButtonPress() {
-    this.setState({ PlayPressed: true });
-    console.log("Play Button Pressed");
   }
   onSearchFormChange(event) {
-    this.setState({ searchTerm: event.target.value });
-  }
-  onSubmitButtonPress() {
-    this.setState({ submitPress: true, word: this.state.searchTerm });
-  }
-  onPlayAgainPress() {
-    this.setState({ choice: "", PlayPressed: false, submitPress: false });
-  }
+    //regex for only entering characters
+    var retest = /^[a-zA-Z]+$/;
 
+    if (event.target.value === "" || retest.test(event.target.value)) {
+      this.setState({ searchTerm: event.target.value });
+      this.setState({ wrongcharacter: true });
+    } else {
+      this.setState({ wrongcharacter: false });
+    }
+  }
+  //checks the user input and if it matched the characters given.
+  onSubmitButtonPress() {
+    var answer = this.props.bestAnswer.toUpperCase();
+
+    var userInput = this.state.searchTerm.toUpperCase();
+    var userInputlength = userInput.length;
+    var breakLoop = false;
+    var count = 0;
+
+    for (var j = 0; j < answer.length + 1; j++) {
+      for (var i = 0; i < userInput.length; i++) {
+        if (userInput.length > 0 && !breakLoop) {
+          if (userInput.charAt(0) === answer.charAt(j)) {
+            console.log(userInput.charAt(0));
+            console.log(answer.charAt(j));
+            userInput =
+              userInput.substring(0, 0) +
+              userInput.substring(1, userInput.length);
+            answer = answer.replace(answer.charAt(j), "");
+            console.log(answer);
+            console.log(userInput);
+            count = count + 1;
+
+            i = 0;
+            j = 0;
+          } else if (userInput === answer && userInput.length === 1) {
+            count = count + 1;
+          } else {
+            /* else if (userInput.charAt(i) === answer.charAt(j)) {
+          character = answer.charAt(j);
+          answer = answer.replace(character, "");
+          count = count + 1;}*/
+            console.log(
+              "i: " +
+                i +
+                " j: " +
+                j +
+                " " +
+                userInput.charAt(i) +
+                " " +
+                answer.charAt(j)
+            );
+          }
+        }
+      }
+
+      if (count === userInputlength) {
+        console.log("Setting wrong input to true :(");
+        this.setState({ wrongInput: true });
+        break;
+      }
+
+      if (userInput.length === 0) {
+        console.log("Setting wrong Input to false :)");
+        this.setState({ wrongInput: false });
+        break;
+      }
+    }
+    console.log(count);
+    console.log(userInputlength);
+    console.log(userInput);
+
+    this.props.submitPressed();
+    this.setState({
+      SubmitPressed: true,
+      word: this.state.searchTerm,
+      len: this.state.searchTerm.length
+    });
+  }
   render() {
+    let wronganswer = this.state.wrongcharacter;
+
     return (
-      <div className="App">
-        <h2>Countdown Game</h2>
-        {this.state.choice === "" ? (
-          <div>
-            <h3>Choose a Difficulty</h3>
-            <button onClick={this.EasyButtonPress}>Easy</button> 60 Second
-            Rounds
-            <hr></hr>
-            <button onClick={this.MediumButtonPress}>Medium</button> 45 Second
-            Rounds
-            <hr></hr>
-            <button onClick={this.HardButtonPress}>Hard</button> 30 Second
-            Rounds
-          </div>
+      <div className="AnswerForm">
+        {this.state.SubmitPressed === true || this.props.isFinished ? (
+          <Congratulations
+            word={this.state.word}
+            wordlength={this.state.len}
+            submitPressed={this.state.SubmitPressed}
+            bestAnswer={this.props.bestAnswer.toUpperCase()}
+            wrongInput={this.state.wrongInput}
+            wrongcharacters={this.state.wrongcharacter}
+            isFinished={this.props.isFinished}
+          />
         ) : (
           <div>
-            {this.state.PlayPressed ? (
+            {!this.props.isFinished && (
               <div>
-                {this.state.submitPress ? (
-                  <div></div>
-                ) : (
-                  <div>
-                    <Timer2 initialSeconds={this.state.seconds} />
-                    <SearchForm
-                      searchTerm={this.state.searchTerm}
-                      onChange={this.onSearchFormChange}
-                    />
-                    <button onClick={this.onSubmitButtonPress}>Submit</button>
-                  </div>
-                )}
-                {this.state.submitPress ? (
-                  <div>
-                    <Definition word={this.state.word} />
-                    <div>
-                      Points: {this.state.word.length}
-                      <div>
-                        <button onClick={this.onPlayAgainPress}>
-                          Play Again
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                ) : (
-                  <div></div>
-                )}
-              </div>
-            ) : (
-              <div>
-                <b>You chose {this.state.choice}</b>
-                <div>
-                  <button onClick={this.onPlayButtonPress}>Play</button>
-                </div>
+                <p>Type in your answer here:</p>
+                <input
+                  type="text"
+                  className="form-control"
+                  maxLength={9}
+                  value={this.state.searchTerm}
+                  onChange={this.onSearchFormChange}
+                  required
+                />
+
+                {wronganswer === false ? (
+                  <p>You have entered an invalid character</p>
+                ) : null}
+                <button
+                  type="button"
+                  className="btn btn-primary btn-lg submit"
+                  onClick={this.onSubmitButtonPress}
+                >
+                  Submit
+                </button>
               </div>
             )}
           </div>
@@ -113,4 +150,4 @@ class App extends Component {
     );
   }
 }
-export default App;
+export default SearchForm;
